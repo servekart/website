266766bbbddee6b7013f2cf864de8f4828d87f08
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2021-09-05 12:05:54 +0530

    update: logo, favicon, contact-email & fix: navbar-scroll issue

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 $(function () {
     $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        var $nav = $(".navbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height() / 3);
     });
-  });
-  // Hamburger Logic
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".navbar__list");
+});
+// Hamburger Logic
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar__list");
 
-  hamburger.addEventListener("click", mobileMenu);
+hamburger.addEventListener("click", mobileMenu);
 
-  function mobileMenu() {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-  }
-  const navLink = document.querySelectorAll(".nav-link");
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
 
-  navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-  function closeMenu() {
-      hamburger.classList.remove("active");
-      navMenu.classList.remove("active");
-  }
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
-  // Form Support
-  (function ($) {
+// Form Support
+(function ($) {
     "use strict";
 
     /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
-    var input2 = $('.validate-input .input101');
+    var input = $(".validate-input .input100");
+    var input2 = $(".validate-input .input101");
 
-    $('.validate-form').on('submit', function (e) {
+    $(".validate-form").on("submit", function (e) {
         var localInput = input.length == 0 ? input2 : input;
         var check = true;
         for (var i = 0; i < localInput.length; i++) {
@@ -46,21 +46,26 @@ $(function () {
         return check;
     });
 
-
-    $('.validate-form .input100').each(function () {
+    $(".validate-form .input100").each(function () {
         $(this).focus(function () {
             hideValidate(this);
         });
     });
 
     function validate(input) {
-        if ($(input).attr('type') == 'email' ) {
-            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if ($(input).attr("type") == "email") {
+            if (
+                $(input)
+                    .val()
+                    .trim()
+                    .match(
+                        /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+                    ) == null
+            ) {
                 return false;
             }
-        }
-        else {
-            if ($(input).val().trim() == '') {
+        } else {
+            if ($(input).val().trim() == "") {
                 return false;
             }
         }
@@ -69,16 +74,12 @@ $(function () {
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
+        $(thisAlert).addClass("alert-validate");
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
+        $(thisAlert).removeClass("alert-validate");
     }
-
-
-
-
 })(jQuery);
